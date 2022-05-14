3a8f0d37e1204b0298de89ff3d98599523adb8e4
Kenyi Hancco <kenyihq@gmail.com>
2022-05-13 23:41:49 -0500

    Se empieza a maquetar las viastas

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crear producto</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Crear producto
+                    </div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="form-group">
+                                <label for="">Descripción</label>
+                                <input type="text" class="form-control" name="description">
+                            </div>
+                            <div class="form-group">
+                                <label for="">Precio</label>
+                                <input type="number" class="form-control" name="price">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                            <a href=" {{ route('products.index') }}" class="btn btn-danger">Cancelar</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
